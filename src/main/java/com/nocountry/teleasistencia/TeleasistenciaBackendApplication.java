2a410f59f51bca23b230d338ca93c51814d8d163
Anthony <anthony7.dev@gmail.com>
2025-10-02 17:43:03 -0500

    Initial commit: setup Spring Boot project

---
+++ src/main/java/com/nocountry/teleasistencia/TeleasistenciaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.teleasistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleasistenciaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleasistenciaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nocountry/teleasistencia/TeleasistenciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.teleasistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleasistenciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
